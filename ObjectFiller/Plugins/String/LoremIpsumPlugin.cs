6d3e3a6de07f23257f6898e30004edf031e093e8
gokTyBalD <gokTyBalD@users.noreply.github.com>
2014-04-23 23:25:21 +0200

    LoremIpsum Plugin implemented

---
+++ ObjectFiller/Plugins/String/LoremIpsumPlugin.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Tynamix.ObjectFiller.Properties;
+
+namespace Tynamix.ObjectFiller.Plugins
+{
+    public class LoremIpsumPlugin : IRandomizerPlugin<string>
+    {
+        private readonly int _wordCount;
+        private readonly string[] _loremIpsumWords;
+
+        public LoremIpsumPlugin(int wordCount)
+        {
+            _wordCount = wordCount;
+            _loremIpsumWords = Resources.loremIpsum.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        /// <summary>
+        /// Gets random data for type <see cref="T"/>
+        /// </summary>
+        /// <returns>Random data for type <see cref="T"/></returns>
+        public string GetValue()
+        {
+            string result = string.Empty;
+            for (int i = 0; i < _wordCount; i++)
+            {
+                result += _loremIpsumWords[i % _loremIpsumWords.Length] + " ";
+            }
+
+            return result;
+        }
+    }
+}
