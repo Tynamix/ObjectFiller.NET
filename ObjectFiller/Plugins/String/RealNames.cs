e8f983d90ff0aa807bf7df2da18fa2d6589034c4
Roman K <Tynamix@users.noreply.github.com>
2014-05-02 12:17:04 +0200

    Update RealNames.cs

--- ObjectFiller/Plugins/String/RealNames.cs
+++ ObjectFiller/Plugins/String/RealNames.cs
@@ -1,53 +1,55 @@
-﻿using System;
+using System;
 using Tynamix.ObjectFiller.Properties;
 
 namespace Tynamix.ObjectFiller
 {
+    public enum RealNameStyle
+    {
+        FirstNameOnly,
+        LastNameOnly,
+        FirstNameLastName,
+        LastNameFirstName
+    }
+
     public class RealNames : IRandomizerPlugin<string>
     {
-        private readonly bool _firstName;
-        private readonly bool _lastName;
-        private readonly bool _fullName;
+        private readonly RealNameStyle _rnStyle;
 
         private string[] _firstNames;
         private string[] _lastNames;
 
-        public RealNames(bool firstName, bool lastName)
+        public RealNames(RealNameStyle realNameStyle)
         {
-            _firstName = firstName;
-            _lastName = lastName;
-            _fullName = firstName && lastName;
+            _rnStyle = realNameStyle;
 
-            if (firstName)
+
+            if (_rnStyle == RealNameStyle.FirstNameLastName
+                || _rnStyle == RealNameStyle.FirstNameOnly)
             {
                 _firstNames = Resources.firstNames.Split(';');
             }
-            if (lastName)
+            if (_rnStyle == RealNameStyle.LastNameFirstName
+                || _rnStyle == RealNameStyle.LastNameOnly)
             {
                 _lastNames = Resources.lastNames.Split(';');
             }
         }
 
-        public RealNames(bool fullName)
-            : this(true, fullName)
-        {
-
-        }
-
         public string GetValue()
         {
-            if (_fullName)
+            if (_rnStyle == RealNameStyle.FirstNameLastName || _rnStyle == RealNameStyle.LastNameFirstName)
             {
                 string firstName = _firstNames[Random.Next(_firstNames.Length)];
                 string lastName = _lastNames[Random.Next(_lastNames.Length)];
 
-                return firstName + " " + lastName;
+
+                return _rnStyle == RealNameStyle.FirstNameLastName ? firstName + " " + lastName : lastName + " " + firstName;
             }
-            if (_firstName)
+            if (_rnStyle == RealNameStyle.FirstNameOnly)
             {
                 return _firstNames[Random.Next(_firstNames.Length)];
             }
-            if (_lastName)
+            if (_rnStyle == RealNameStyle.LastNameOnly)
             {
                 return _lastNames[Random.Next(_lastNames.Length)];
             }
