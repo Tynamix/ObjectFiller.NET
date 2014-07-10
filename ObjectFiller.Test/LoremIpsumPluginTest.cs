1412319f2ebabd827fea4680e4748d2f49d46f93
gokTyBalD <gokTyBalD@users.noreply.github.com>
2014-07-10 19:55:00 +0200

    unit test

--- ObjectFiller.Test/LoremIpsumPluginTest.cs
+++ ObjectFiller.Test/LoremIpsumPluginTest.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ObjectFiller.Test.TestPoco.Library;
 using Tynamix.ObjectFiller;
@@ -25,6 +26,19 @@ namespace ObjectFiller.Test
             Assert.IsNotNull(book.Name);
             Assert.AreEqual(isbnWordCount, book.ISBN.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Length);
             Assert.AreEqual(nameWordCount, book.Name.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Length);
+        }
+
+        [TestMethod]
+        public void TestNewLoremIpsum()
+        {
+            Filler<Book> book = new Filler<Book>();
+            book.Setup()
+                .OnProperty(x => x.ISBN).Use(new Lipsum(LipsumFlavor.InDerFremde, paragraphs: 3));
+
+            var b = book.Create();
+
+            Assert.IsNotNull(b);
+            Assert.AreEqual(3, b.ISBN.Split('\n').Length -1);
         }
     }
 }
