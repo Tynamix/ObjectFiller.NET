ed139a5a205b2b550e298ba6c07f3650070893ee
unknown <roman.koehler@NB163.saxsys.de>
2015-04-29 18:29:19 +0200

    Randomizer implemented

---
+++ ObjectFiller.Test/RandomizerTest.cs
@@ -0,0 +1,51 @@
+﻿namespace ObjectFiller.Test
+{
+    using System;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using ObjectFiller.Test.TestPoco.Library;
+    using ObjectFiller.Test.TestPoco.Person;
+
+    using Tynamix.ObjectFiller;
+
+    [TestClass]
+    public class RandomizerTest
+    {
+        [TestMethod]
+        public void GetRandomInt()
+        {
+            var number = Randomizer<int>.Create(new IntRange(1, 2));
+
+            Assert.IsTrue(number == 1 || number == 2);
+        }
+
+        [TestMethod]
+        public void FillAllAddressProperties()
+        {
+            var a = Randomizer<Address>.Create();
+            Assert.IsNotNull(a.City);
+            Assert.IsNotNull(a.Country);
+            Assert.AreNotEqual(0, a.HouseNumber);
+            Assert.IsNotNull(a.PostalCode);
+            Assert.IsNotNull(a.Street);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void TryingToCreateAnObjectWithAnInterfaceShallFailAndHaveAnInnerexception()
+        {
+            try
+            {
+                Randomizer<Library>.Create();
+            }
+            catch (InvalidOperationException ex)
+            {
+                Assert.IsNotNull(ex.InnerException);
+                throw;
+            }
+            
+        }
+
+    }
+}
---
+++ ObjectFiller/Randomizer.cs
@@ -0,0 +1,67 @@
+﻿namespace Tynamix.ObjectFiller
+{
+    using System;
+    using System.Linq;
+
+    /// <summary>
+    /// This class is a easy way to get random values. 
+    /// </summary>
+    /// <typeparam name="T">Target type of whom the random value shall been generated</typeparam>
+    public static class Randomizer<T>
+    {
+        /// <summary>
+        /// The default setup item
+        /// </summary>
+        private static readonly FillerSetupItem setup;
+
+        /// <summary>
+        /// Initializes static members of the <see cref="Randomizer{T}"/> class.
+        /// </summary>
+        static Randomizer()
+        {
+            setup = new FillerSetupItem();
+        }
+
+        /// <summary>
+        /// Creates a random value of type <see cref="T"/>.
+        /// </summary>
+        /// <returns>A value of type <see cref="T"/></returns>
+        public static T Create()
+        {
+            Type targetType = typeof(T);
+            if (!setup.TypeToRandomFunc.ContainsKey(targetType))
+            {
+                if (targetType.IsClass)
+                {
+                    var fillerType = typeof(Filler<>).MakeGenericType(typeof(T));
+                    var oFiller = Activator.CreateInstance(fillerType);
+                    var methodInfo = oFiller.GetType().GetMethods().Single(x => !x.GetParameters().Any() && x.Name == "Create");
+
+                    try
+                    {
+                        return (T)methodInfo.Invoke(oFiller, null);
+                    }
+                    catch (Exception ex)
+                    {
+                        throw new InvalidOperationException("The type " + typeof(T).FullName + " needs additional information to get created. " +
+                            "Please use the Filler class and call \"Setup\" to create a setup for that type. See Innerexception for more details.", ex);
+                    }
+                }
+
+                return default(T);
+            }
+
+            return (T)setup.TypeToRandomFunc[typeof(T)]();
+        }
+
+        /// <summary>
+        /// Creates a random value of the target type. It will use a <see cref="IRandomizerPlugin{T}"/> for that
+        /// </summary>
+        /// <param name="randomizerPlugin"><see cref="IRandomizerPlugin{T}"/> to use</param>
+        /// <returns>A random value of type <see cref="T"/></returns>
+        public static T Create(IRandomizerPlugin<T> randomizerPlugin)
+        {
+            return randomizerPlugin.GetValue();
+        }
+    }
+}
