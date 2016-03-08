9579bfbbc3da154eac1b1d7c805df92a4d4b042b
Roman Köhler <romanumfrage@yahoo.de>
2016-03-08 23:37:22 +0100

    #70 Collectionizer implemented

---
+++ Tynamix.ObjectFiller.Test/CollectionizerTest.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Tynamix.ObjectFiller;
+using Xunit;
+
+namespace ObjectFiller.Test
+{
+    public class CollectionizerPoco
+    {
+        public IEnumerable<string> MnemonicStrings { get; set; }
+
+        public List<int> IntRange { get; set; }
+    }
+
+    public class CollectionizerTest
+    {
+        [Fact]
+        public void TestMnemonicStringPlugin()
+        {
+            var filler = new Filler<CollectionizerPoco>();
+
+            filler.Setup()
+                .OnProperty(x => x.MnemonicStrings)
+                .Use(new Collectionizer<string, MnemonicString>(new MnemonicString(1, 20, 25), 3, 10));
+
+            var collection = filler.Create();
+            Assert.True(collection.MnemonicStrings.Count() >= 3 && collection.MnemonicStrings.Count() <= 10);
+            Assert.True(collection.MnemonicStrings.All(x => x.Length >= 20 && x.Length <= 25));
+        }
+
+        [Fact]
+        public void TestIntRangePlugin()
+        {
+            var filler = new Filler<CollectionizerPoco>();
+
+            filler.Setup()
+                .OnProperty(x => x.IntRange)
+                .Use(new Collectionizer<int, IntRange>(new IntRange(10, 15), 3, 10));
+
+            var collection = filler.Create();
+            Assert.True(collection.IntRange.Count >= 3 && collection.IntRange.Count() <= 10);
+            Assert.True(collection.IntRange.All(x => x >= 10 && x <= 15));
+        }
+    }
+}
---
+++ Tynamix.ObjectFiller/Plugins/Collectionizer.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections.Generic;
+
+namespace Tynamix.ObjectFiller
+{
+    /// <summary>
+    /// The collectionizer can be used to create <see cref="List{T}"/> based on a <see cref="IRandomizerPlugin{T}"/>.
+    /// </summary>
+    /// <typeparam name="T">Typeparameter of of the target List</typeparam>
+    /// <typeparam name="TRandomizer">Plugin which will be used to create the List</typeparam>
+    public class Collectionizer<T, TRandomizer> : IRandomizerPlugin<List<T>>
+        where TRandomizer : IRandomizerPlugin<T>, new()
+    {
+        private readonly IRandomizerPlugin<T> randomizerToUse;
+        private readonly int minCount;
+        private readonly int maxCount;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Collectionizer{T,TRandomizer}"/> class.
+        /// </summary>
+        public Collectionizer()
+            : this(new TRandomizer())
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Collectionizer{T,TRandomizer}"/> class.
+        /// </summary>
+        /// <param name="minCount">Min count of list items</param>
+        public Collectionizer(uint minCount)
+            : this(new TRandomizer(), minCount)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Collectionizer{T,TRandomizer}"/> class.
+        /// </summary>
+        /// <param name="minCount">Min count of list items</param>
+        /// <param name="maxCount">Max count of list items</param>
+        public Collectionizer(uint minCount, uint maxCount)
+            : this(new TRandomizer(), minCount, maxCount)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Collectionizer{T,TRandomizer}"/> class.
+        /// </summary>
+        /// <param name="randomizerToUse">The randomizer which will be used. Use this if you want
+        /// to specifiy how the randomizer to use should work</param>
+        public Collectionizer(TRandomizer randomizerToUse)
+            : this(randomizerToUse, 10)
+        {
+
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Collectionizer{T,TRandomizer}"/> class.
+        /// </summary>
+        /// <param name="randomizerToUse">The randomizer which will be used. Use this if you want
+        /// to specifiy how the randomizer to use should work</param>
+        /// <param name="minCount">Min count of list items</param>
+        public Collectionizer(TRandomizer randomizerToUse, uint minCount)
+            : this(randomizerToUse, minCount, 50)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Collectionizer{T,TRandomizer}"/> class.
+        /// </summary>
+        /// <param name="randomizerToUse">The randomizer which will be used. Use this if you want
+        /// to specifiy how the randomizer to use should work</param>
+        /// <param name="minCount">Min count of list items</param>
+        /// <param name="maxCount">Max count of list items</param>
+        public Collectionizer(TRandomizer randomizerToUse, uint minCount, uint maxCount)
+        {
+            if (randomizerToUse == null)
+            {
+                randomizerToUse = new TRandomizer();
+            }
+
+            if (minCount > maxCount)
+            {
+                maxCount = minCount;
+                minCount = maxCount;
+            }
+
+            this.randomizerToUse = randomizerToUse;
+            this.minCount = (int)minCount;
+            this.maxCount = (int)maxCount;
+        }
+
+
+        /// <summary>
+        /// Gets random data for type <see cref="T"/>
+        /// </summary>
+        /// <returns>Random data for type <see cref="T"/></returns>
+        public List<T> GetValue()
+        {
+            var count = Randomizer<int>.Create(new IntRange(this.minCount, this.maxCount));
+            List<T> result = new List<T>();
+
+            for (int i = 0; i < count; i++)
+            {
+                result.Add(this.randomizerToUse.GetValue());
+            }
+
+            return result;
+        }
+    }
+}
