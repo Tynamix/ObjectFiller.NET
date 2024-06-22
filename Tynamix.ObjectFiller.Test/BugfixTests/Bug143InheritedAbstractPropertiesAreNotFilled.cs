66a1451b7a5c2191f9f75397aa6b3b360bb7cf53
Clément Guyonnet <cguyonnet@users.noreply.github.com>
2024-06-22 02:52:51 +0200

    #143 Support filling inherited abstract properties

---
+++ Tynamix.ObjectFiller.Test/BugfixTests/Bug143InheritedAbstractPropertiesAreNotFilled.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tynamix.ObjectFiller.Test.BugfixTests
+{
+    internal abstract class EntityBase
+    {
+        public string Name { get; set; }
+        public abstract string AbstractName { get; set; }
+    }
+
+    internal class CustomEntity : EntityBase
+    {
+        public override string AbstractName { get; set; }
+    }
+
+    [TestClass]
+    public class Bug143InheritedAbstractPropertiesAreNotFilled
+    {
+        [TestMethod]
+        public void InheritedAbstractPropertiesShouldBeFilled()
+        {
+            Filler<CustomEntity> filler = new Filler<CustomEntity>();
+            filler
+                .Setup(true)
+                .OnProperty(x => x.Name).Use("Alice")
+                .OnProperty(x => x.AbstractName).Use("John");
+            var entity = filler.Create();
+            
+            Assert.IsNotNull(entity);
+            Assert.AreEqual("Alice", entity.Name);
+            Assert.AreEqual("John", entity.AbstractName);
+        }
+    }
+}
